1592e22643b3d3131cb6e77984c990037f48c29d
AhmadMtr <ahmadmtr@protonmail.com>
2023-11-22 00:48:35 +0300

    finish prototype

---
+++ cmake-build-debug/CMakeFiles/LinkSort.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LinkSort.dir/main.cpp.obj"
+  "CMakeFiles/LinkSort.dir/main.cpp.obj.d"
+  "LinkSort.exe"
+  "LinkSort.exe.manifest"
+  "LinkSort.pdb"
+  "libLinkSort.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LinkSort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/LinkSort.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for LinkSort.
+# This may be replaced when dependencies are built.
